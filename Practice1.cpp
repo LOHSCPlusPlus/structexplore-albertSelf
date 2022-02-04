e1b4558c125e6d5d4905506b063fb169d08bca42
ALBERTSELF <>
2022-02-04 16:36:01 +0000

    Practice1 working

--- Practice1.cpp
+++ Practice1.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 // Put the person struct here
 
-
+struct Name {
+  string first;
+  string last;
+};
 
 
 
@@ -11,13 +15,18 @@ using namespace std;
 int main() {
 	// Create two instances of person here, and assign the names.
 
+  Name person1;
+  person1.first = "Dorian";
+  person1.last = "Gray";
 
-
-
-
+  Name person2;
+  person2.first = "Charlie";
+  person2.last = "Brown";
 
 	// Add the prints here
 
+  cout << person1.first << " " << person1.last << endl;
+  cout << person2.first << " " << person2.last << endl;
 
 
 
